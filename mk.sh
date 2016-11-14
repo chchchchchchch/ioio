15d144519e014afdaac2646585cda5a13c501e5b
christoph <christoph@lafkon.net>
2016-11-14 11:42:35 +0100

    major update

--- mk.sh
+++ mk.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
- OUTPUTDIR=FREEZE
- SVGFOLDERS=tmp
+# TODO: SVGHEADER not as variable
+
+  OUTDIR=_
+
+
+# --------------------------------------------------------------------------- #
+# VALIDATE (PROVIDED) INPUT 
+# --------------------------------------------------------------------------- #
+  if [ `echo $* | wc -c` -lt 2 ]; then echo "No arguments provided"; exit 0;
+  else if [ `ls \`ls ${1}* 2> /dev/null\` 2> /dev/null  | #
+             grep "\.svg$" | wc -l` -lt 1 ];              #
+        then echo 'No valid svg!'; exit 0;                #    
+      # else echo -e "PROCESSING NOW:\n${1}* \n--------------------";
+    fi
+  fi
 
 # =========================================================================== #
-  for SVG in `find $SVGFOLDERS -name "*.svg"`
-   do
+# DO IT NOW!
 # =========================================================================== #
-
-   OUTPUTBASE=`basename $SVG   | \
-               cut -d "_" -f 2 | \
-               sed 's/-R+//g'  | \
-               tr -t [:lower:] [:upper:]`
-
+  for SVG in `ls \`ls ${1}*\` | grep "\.svg$"`
+   do OUTPUTBASE=`basename $SVG            | #
+                  cut -d "_" -f 2          | #
+                  sed 's/-R+//g'           | #
+                  tr -t [:lower:] [:upper:]` #
 # --------------------------------------------------------------------------- #
 # MOVE ALL LAYERS ON SEPARATE LINES IN A TMP FILE
 # --------------------------------------------------------------------------- #
@@ -24,7 +35,6 @@
   sed 's/display:none/display:inline/g'  | # MAKE VISIBLE EVEN WHEN HIDDEN
   grep -v 'label="XX_'                   | # REMOVE EXCLUDED LAYERS
   sed 's/<\/svg>/\n&/g'                  | # CLOSE TAG ON SEPARATE LINE
-# tr -s ' '                              | # CLEAN CONSECUTIVE SPACES
   sed "s/^[ \t]*//"                      | # REMOVE LEADING BLANKS
   tr -s ' '                              | # REMOVE CONSECUTIVE BLANKS
   tee > ${SVG%%.*}.tmp                     # WRITE TO TEMPORARY FILE
@@ -32,41 +42,34 @@
 # --------------------------------------------------------------------------- #
 # GENERATE CODE FOR FOR-LOOP TO EVALUATE COMBINATIONS
 # --------------------------------------------------------------------------- #
-
   # RESET (IMPORTANT FOR 'FOR'-LOOP)
-  LOOPSTART=""
-  VARIABLES=""
-  LOOPCLOSE=""  
+  LOOPSTART="";VARIABLES="";LOOPCLOSE="";CNT=0
 
-  CNT=0  
-  for BASETYPE in `sed ':a;N;$!ba;s/\n/ /g' ${SVG%%.*}.tmp | \
-                   sed 's/scape:label/\nlabel/g'           | \
-                   grep ^label                             | \
-                   cut -d "\"" -f 2                        | \
-                   cut -d "-" -f 1                         | \
+  for BASETYPE in `sed ':a;N;$!ba;s/\n/ /g' ${SVG%%.*}.tmp | #
+                   sed 's/<g/\n&/g'                  | # GROUPS ON NEWLINE
+                   sed '/^<g/s/>/&\n/g'              | # FIRST ON '>' ON NEWLINE
+                   grep ':groupmode="layer"'         | #
+                   sed '/^<g/s/scape:label/\nlabel/' | #
+                   grep ^label                       | #
+                   cut -d "\"" -f 2                  | #
+                   cut -d "-" -f 1                   | #
                    sort -u`
    do
-      ALLOFTYPE=`sed ':a;N;$!ba;s/\n/ /g' ${SVG%%.*}.tmp   | \
-                 sed 's/scape:label/\nlabel/g'             | \
-                 grep ^label                               | \
-                 cut -d "\"" -f 2                          | \
-                 grep $BASETYPE`
-
-      LOOPSTART=${LOOPSTART}"for V$CNT in $ALLOFTYPE; do "
-      VARIABLES=${VARIABLES}'$'V${CNT}" "
-      LOOPCLOSE=${LOOPCLOSE}"done; "
-
-      CNT=`expr $CNT + 1`
-
+       ALLOFTYPE=`sed ':a;N;$!ba;s/\n/ /g' ${SVG%%.*}.tmp  | #
+                  sed 's/scape:label/\nlabel/g'            | #
+                  grep ^label                              | #
+                  cut -d "\"" -f 2                         | #
+                  grep $BASETYPE`                            #
+       LOOPSTART=${LOOPSTART}"for V$CNT in $ALLOFTYPE; do "
+       VARIABLES=${VARIABLES}'$'V${CNT}" "
+       LOOPCLOSE=${LOOPCLOSE}"done; "
+       CNT=`expr $CNT + 1`
   done
 
 # --------------------------------------------------------------------------- #
 # EXECUTE CODE FOR FOR-LOOP TO EVALUATE COMBINATIONS
 # --------------------------------------------------------------------------- #
-
   KOMBILIST=kombinationen.list ; if [ -f $KOMBILIST ]; then rm $KOMBILIST ; fi
-
-# echo ${LOOPSTART}" echo $VARIABLES >> $KOMBILIST ;"${LOOPCLOSE}
   eval ${LOOPSTART}" echo $VARIABLES >> $KOMBILIST ;"${LOOPCLOSE}
 
 # --------------------------------------------------------------------------- #
@@ -78,73 +81,51 @@
   for KOMBI in `cat $KOMBILIST | sed 's/ /DHSZEJDS/g'`
    do
       KOMBI=`echo $KOMBI | sed 's/DHSZEJDS/ /g'`
-
-#     R=`basename $SVG | cut -d "_" -f 2 | grep "R+" |  cut -d "-" -f 2`
-#     if [ A$R = "AR+" ]; then R="-R+"; else R= ; fi
-#     IOS=`basename $SVG | cut -d "_" -f 3-`
-#   # OSVG=$OUTPUTDIR/${OUTPUTBASE}`echo ${KOMBI}${IOS} | \
-#   #                               md5sum | cut -c 1-6 | \
-#   #                               tr -t [:lower:] [:upper:]`${R}_${IOS}
-
-#     BASEID=`echo ${OUTPUTBASE} | md5sum | cut -c 1-8 | tr -t [:lower:] [:upper:]`
-#     OSVG=${BASEID}`echo ${KOMBI}${IOS} | \
-#                    md5sum | cut -c 1-6 | \
-#                    tr -t [:lower:] [:upper:]`
-#     OSVG=$OUTPUTDIR/`echo $OSVG${R} | rev | cut -c 1-14 | rev`_${IOS}
-#     echo $OSVG
-
         R=`basename $SVG | cut -d "_" -f 2 | #
            grep "R+" | cut -d "-" -f 2`
       IOS=`basename $SVG | cut -d "_" -f 3-`
       if [ A$R = "AR+" ]; then R="+R-"; else R= ; fi
-      NID=`echo ${OUTPUTBASE} | #
-           cut -d "-" -f 1 | #
+      NID=`echo ${OUTPUTBASE}        | #
+           cut -d "-" -f 1           | #
            tr -t [:lower:] [:upper:] | #
-           md5sum | cut -c 1-4 | #
-           tr -t [:lower:] [:upper:]`
-      FID=`basename $SVG | #
+           md5sum | cut -c 1-4       | #
+           tr -t [:lower:] [:upper:]`  #
+      FID=`basename $SVG             | #
            tr -t [:lower:] [:upper:] | #
-           md5sum | cut -c 1-4 | #
-           tr -t [:lower:] [:upper:]`
-      DIF=`echo ${KOMBI}${IOS} | #
-           md5sum | cut -c 1-9 | #
-           tr -t [:lower:] [:upper:] | rev`
+           md5sum | cut -c 1-4       | #
+           tr -t [:lower:] [:upper:]`  #
+      DIF=`echo ${KOMBI}${IOS}       | #
+           md5sum | cut -c 1-9       | #
+           tr -t [:lower:] [:upper:] | #
+           rev`                        #
 
-     OSVG=$OUTPUTDIR/$NID$FID`echo $R$DIF | cut -c 1-9 | rev`_${IOS}
-     echo $OSVG
+      SVGOUT=$OUTDIR/$NID$FID`echo $R$DIF | cut -c 1-9 | rev`_${IOS}
+      echo "WRITING: $SVGOUT"
 
-
-     if [ ! -f $OSVG ]; then
-
-      echo "$SVGHEADER"                                   >  $OSVG
-
-      for LAYERNAME in `echo $KOMBI`
-        do
-          grep -n "label=\"$LAYERNAME\"" ${SVG%%.*}.tmp   >> ${OSVG%%.*}.tmp
+      echo "$SVGHEADER"                                  >  $SVGOUT
+      for  LAYERNAME in `echo $KOMBI`
+        do grep -n "label=\"$LAYERNAME\"" ${SVG%%.*}.tmp >> ${SVGOUT}.tmp
       done
-
-      cat ${OSVG%%.*}.tmp | sort -n | cut -d ":" -f 2-    >> $OSVG
-      echo "</svg>"                                       >> $OSVG
-      rm ${OSVG%%.*}.tmp
+      cat ${SVGOUT}.tmp | sort -n | cut -d ":" -f 2-     >> $SVGOUT
+      echo "</svg>"                                      >> $SVGOUT
+      rm ${SVGOUT}.tmp
 
     # DO SOME CLEAN UP
-    # -----------------------------------------  #
-      inkscape --vacuum-defs              $OSVG  # INKSCAPES VACUUM CLEANER
-      NLFOO=Nn${RANDOM}lL                        # SET RANDOM PLACEHOLDER
-      sed -i ":a;N;\$!ba;s/\n/$NLFOO/g"   $OSVG  # PLACEHOLDER FOR LINEBREAKS
+    # -------------------------------------------  #
+      inkscape --vacuum-defs              $SVGOUT  # INKSCAPES VACUUM CLEANER
+      NLFOO=Nn${RANDOM}lL                          # SET RANDOM PLACEHOLDER
+      sed -i ":a;N;\$!ba;s/\n/$NLFOO/g"   $SVGOUT  # PLACEHOLDER FOR LINEBREAKS
       sed -i -e "s,<defs,\nXXX<defs,g"    \
              -e "s,</defs>,</defs>\n,g"   \
-             -e "/^<defs/s/\/>/&\n/g"     $OSVG  # FORMAT DEFS AND MARK
+             -e "/^<defs/s/\/>/&\n/g"     $SVGOUT  # FORMAT DEFS AND MARK
       sed -i -e "s,<sodipodi,\nXXX&,g"    \
              -e "s,</sodipodi>,&\n,g"     \
-             -e "/^<sodipodi/s/\/>/&\n/g" $OSVG  # FORMAT SODIPODI STUFF AND MARK
+             -e "/^<sodipodi/s/\/>/&\n/g" $SVGOUT  # FORMAT SODIPODI STUFF AND MARK
       sed -i -e "s,<metadata,\nXXX&,g"    \
-             -e "s,</metadata>,&\n,g"     $OSVG  # FORMAT METADATA AND MARK
-      sed -i "/^XXX/s/^.*$//g"            $OSVG  # DELETE MARKED LINES
-      sed -i "s/$NLFOO/\n/g"              $OSVG  # RESTORE LINEBREAKS
-      sed -i '/^$/d'                      $OSVG  # DELETE EMPTY LINES
-
-     fi
+             -e "s,</metadata>,&\n,g"     $SVGOUT  # FORMAT METADATA AND MARK
+      sed -i "/^XXX/s/^.*$//g"            $SVGOUT  # DELETE MARKED LINES
+      sed -i "s/$NLFOO/\n/g"              $SVGOUT  # RESTORE LINEBREAKS
+      sed -i '/^$/d'                      $SVGOUT  # DELETE EMPTY LINES
 
   done
 
